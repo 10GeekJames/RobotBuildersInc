1682dfcfde54f6e727b6669eaf53eaf01fa045da
JamesK <James@10-geek.com>
2024-06-04 17:20:25 -0500

    flatened structure do not pull KP!!!!

--- RobotBuildersInc/src/components/Shared/SectionBadge.jsx
+++
@@ -1,7 +0,0 @@
-const SectionBadge = ({title}) => (
-  <div className="badge badge-lg h-12 badge-outline p-4 rounded-lg font-semibold text-2xl" >
-    <p>{title}</p>
-  </div>
-);
-
-export default SectionBadge;
