30ba9a98c0c518e14722233373222cd1ee6323a0
KPInc425 <kpinc425@gmail.com>
2024-06-03 20:43:51 -0700

    Update Hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
     <div className="hero-content text-neutral-content w-full py-16">
       <div className="flex flex-col lg:flex-row w-full gap-8">
         <div className="max-w-md flex flex-col items-center text-center mx-auto lg:items-start lg:text-start justify-center gap-4">
-          <div className="badge badge-lg h-12 badge-outline p-4 rounded-lg font-semibold" >Prototype Faster</div>
+          <div className="badge badge-lg h-12 badge-outline p-4 rounded-lg font-semibold">Your Vision, Our Expertise</div>
           <GradientTitle title="Take your idea to the next level." additionalClasses="leading-[1]" />
           <p className="mb-5">Customized solutions to turn your ideas into powerful applications. We bring together creativity, technology, and expertise to deliver exceptional results.</p>
-          <p className="mb-5">We transform your business challenges into custom software solutions. Let us help you streamline processes and boost performance with our expert development services.</p>
+          {/* <p className="mb-5">We transform your business challenges into custom software solutions. Let us help you streamline processes and boost performance with our expert development services.</p> */}
           <button className="btn btn-primary w-1/3">Fulfill Dream</button>
           <p className="text-sm text-neutral-content-lighter">* We don't bite</p>
         </div>
