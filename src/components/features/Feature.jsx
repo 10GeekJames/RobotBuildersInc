94bb052997535226376e8a348f0d93f1c2eefda0
KPInc425 <kpinc425@gmail.com>
2024-06-03 20:41:24 -0700

    Refactor features components

--- src/components/features/Feature.jsx
+++ src/components/features/Feature.jsx
@@ -12,7 +12,12 @@ const Feature = ({
 }) => {
   const getGradient = () => {
     if (hasGradient) {
-      if (startGradient && endGradient) {
+      if (
+        startGradient &&
+        endGradient &&
+        startGradient !== "" &&
+        endGradient !== ""
+      ) {
         return `bg-gradient-to-t ${"from-" + startGradient} ${
           "to-" + endGradient
         }`;
@@ -24,18 +29,20 @@ const Feature = ({
   };
   return (
     <div
-      className={`${reverse ? "lg:rounded-[75%_25%/20%]" : "lg:rounded-[25%_75%/20%]"} ${reverse ? "lg:flex-row" : "lg:flex-row-reverse"} flex flex-col gap-4 lg:justify-center lg:items-center text-start rounded-3xl p-4 lg:p-12 ${getGradient()} shadow-[inset_0_0_50px_oklch(var(--nc)),0_0_50px_oklch(var(--nc))]`}
+      className={`grid grid-cols-2 gap-4 lg:justify-center lg:items-center text-start rounded-3xl p-4 lg:p-12  shadow-[inset_0_0_50px_oklch(var(--nc)),0_0_50px_oklch(var(--nc))] ${getGradient()} ${
+        reverse ? "lg:rounded-[75%_25%/20%]" : "lg:rounded-[25%_75%/20%]"
+      } ${reverse ? "lg:flex-row" : "lg:flex-row-reverse"} `}
     >
       <div
-        className={`flex flex-col w-full ${
-          reverse ? "items-end" : "items-start"
-        } lg:items-center`}
+        className={`flex flex-col gap-4 w-full lg:items-center ${
+          reverse ? "items-end order-1" : "items-start order-2"
+        }`}
       >
         <GradientTitle title={title} />
         <div
-          className={`flex bg-[radial-gradient(ellipse_at_center,_rgba(255,255,255,0)_0%,_rgba(255,255,255,1)_70%,_rgba(255,255,255,1)_100%);] w-3/4 lg:w-full ${
+          className={`flex bg-[radial-gradient(ellipse_at_center,_rgba(255,255,255,0)_0%,_rgba(255,255,255,1)_70%,_rgba(255,255,255,1)_100%);] w-3/4 lg:w-full shadow-[inset_0_0_50px_oklch(var(--nc)),0_0_50px_oklch(var(--nc))] ${
             reverse ? "rounded-[75%_25%/20%]" : "rounded-[25%_75%/20%]"
-          } shadow-[inset_0_0_50px_oklch(var(--nc)),0_0_50px_oklch(var(--nc))]`}
+          }`}
         >
           <img
             src={image}
@@ -46,7 +53,7 @@ const Feature = ({
           />
         </div>
       </div>
-      <div className={`${reverse ? "text-end" : ""}`}>
+      <div className={`${reverse ? "text-end order-2" : "order-1"}`}>
         <GradientTitle title={subTitle} />
         <p className="text-xl pt-8">{description}</p>
       </div>
--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -2,6 +2,7 @@ import GradientTitle from "../shared/GradientTitle";
 import SectionBadge from "../shared/SectionBadge";
 import SectionWrapper from "../shared/SectionWrapper";
 import Feature from "./Feature";
+import featureData from "../../data/featureData";
 
 const Features = () => {
   return (
@@ -9,60 +10,27 @@ const Features = () => {
       <div className="flex flex-col gap-4 justify-center items-center mb-16">
         <SectionBadge title="Services" />
         <div className="max-w-screen-lg mx-auto">
-          <GradientTitle title="We can build your dream product into a cross-platform juggernaut" textSize="lg" fontWeight="medium" />
+          <GradientTitle
+            title="We can build your dream product into a cross-platform juggernaut"
+            textSize="lg"
+            fontWeight="medium"
+          />
         </div>
       </div>
       <div className="flex flex-col gap-24">
-        <Feature
-          title="Custom Software"
-          subTitle="Transforming Ideas into Reality"
-          image="./Feature1.jpg"
-          description="Customized solutions to turn your ideas into powerful applications. We bring together creativity, technology, and expertise to deliver exceptional results."
-          reverse={false}
-          hasGradient={true}
-          // startGradient={"primary/50"}
-          // endGradient={"secondary/50"}
-        />
-        <Feature
-          title="Blazor Development"
-          subTitle="Full-Stack .NET Applications"
-          image="./Feature1.jpg"
-          description="Our .NET Blazor development services empower us to craft dynamic web applications with the robustness of C#. Leveraging the Blazor framework, we deliver full-stack solutions that excel in performance and scalability. Whether you require a web app from scratch or seek to enhance an existing one, our expertise ensures your application meets and exceeds the highest standards."
-          reverse={true}
-          hasGradient={true}
-          // startGradient={"primary/50"}
-          // endGradient={"secondary/50"}
-        />
-        <Feature
-          title="Blazor WebAssembly (WASM)"
-          subTitle="Native-Like Web Experiences"
-          image="./Feature1.jpg"
-          description="With Blazor WebAssembly, we create wildly interactive web applications that redefine user experiences. By harnessing the power of WebAssembly, we deliver near-native performance, guaranteeing a seamless and engaging application for your users. Our in-shop custom development platform provides projects industry strength security, secure application lazy-loading, and multitenant support by the end of the first week."
-          reverse={false}
-          hasGradient={true}
-          // startGradient={"primary/50"}
-          // endGradient={"secondary/50"}
-        />
-        <Feature
-          title=".NET MAUI Development"
-          subTitle="One Codebase, Every Platform"
-          image="./Feature1.jpg"
-          description=".NET MAUI enables us to develop cross-platform applications that offer a consistent user experience across various devices such as Windows, Mac, Android, and iOS/Apple."
-          reverse={true}
-          hasGradient={true}
-          // startGradient={"primary/50"}
-          // endGradient={"secondary/50"}
-        />
-        <Feature
-          title="Microsoft Project Rehab & Rescue"
-          subTitle="Turning Challenges into Opportunities"
-          image="./Feature1.jpg"
-          description="We specialize in rehabilitating and rescuing Microsoft projects. With our expertise, we can identify issues, implement best practices, and get your project back on track. Whether you're facing challenges with architecture, performance, or scalability, we have the experience to turn things around and ensure successful project delivery."
-          reverse={true}
-          hasGradient={true}
-          // startGradient={"primary/50"}
-          // endGradient={"secondary/50"}
-        />
+        {featureData.map((feature, index) => (
+          <Feature
+            key={feature.title}
+            title={feature.title}
+            subTitle={feature.subTitle}
+            image={feature.image}
+            description={feature.description}
+            reverse={index % 2 === 0 ? true : false}
+            hasGradient={feature.hasGradient}
+            startGradient={feature.startGradient}
+            endGradient={feature.endGradient}
+          />
+        ))}
       </div>
     </SectionWrapper>
   );
